c2d873afc5815bf284147b110bed1f57dab442a9
zsf1482451437 <1482451437@qq.com>
2025-03-02 16:34:39 +0800

    fix: circle

--- src/components/CircleLayout/index.tsx
+++ src/components/CircleLayout/index.tsx
@@ -2,23 +2,26 @@ import React from 'react';
 import { createCn } from '@/utils/cn';
 import styles from './index.module.scss';
 
+
 const cn = createCn(styles);
+
+const RADIUS = 3;
 
 interface CircleLayoutProps {
   children: React.ReactNode;
 }
 
 const CircleLayout: React.FC<CircleLayoutProps> = ({ children }) => {
-  const childrenCount = React.Children.count(children);
-
   return (
     <div className={cn('container')}>
       {React.Children.map(children, (child, index) => {
-        const angle = (360 / childrenCount) * index; // 计算每个子元素的角度
+        const angle = (index * 60) * (Math.PI / 180); // 计算每个子元素的角度
+        const x = RADIUS * Math.cos(angle) * 50; // 计算x坐标
+        const y = RADIUS * Math.sin(angle) * 50; // 计算y坐标
         return (
           <div
             className={cn('item')}
-            style={{ transform: `rotate(${angle}deg) translateY(-50%)` }}
+            style={{ transform: `translate(-50%,-50%) translate(${x}px, ${y}px)` }}
           >
             {child}
           </div>
